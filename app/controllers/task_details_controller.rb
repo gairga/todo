521de5f4f39c4e0a04ed89ebb2b532a503be8a5e
Gabriela Ilarreta <gabriela.ilarreta@globant.com>
2018-11-04 15:31:32 -0300

    Index Task

--- app/controllers/task_details_controller.rb
+++ app/controllers/task_details_controller.rb
@@ -14,20 +14,6 @@ class TaskDetailsController < ApplicationController
   end
 end
 
-def destroy
-  @completed_task = current_user.tasks.find(params[:task_id])
-  @completed_task.completed_tasks[0].destroy
-  redirect_to root_path, notice: 'La tarea fue eliminada de la lista'
-end
-
-def show
-  @completed_task = Task.find(params[:task_id])
-  @completed_task.completed_tasks[0]
-  @completed_tasks = @completed_task.users
-  @completed_tasks_ranked = Task.find(params[:task_id])
-  @completed_tasks_ranked = @completed_tasks_ranked.completed_tasks.order(:created_at).limit(5)
-  @completed_tasks_ranked
-end
 
 
 end
